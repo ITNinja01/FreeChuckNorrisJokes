df0a3deb020a856e1f5c27b86879002ccc8429c0
David Willson <dwillson@peckham.org>
2025-12-31 07:29:22 -0500

    Was missing .ps1s in the psm1 and had formating issues with files in the.psd1. I was able to publish. Vibe coding was not my friend here.

---
+++ FreeChuckNorrisJokes.psm1
@@ -0,0 +1,4 @@
+. $PSScriptRoot\Functions\Get-ChuckNorrisJoke.ps1
+Export-ModuleMember -Function Get-ChuckNorrisJoke
+. $PSScriptRoot\Functions\Get-ChuckNorrisJokeByCategory.ps1
+Export-ModuleMember -Function Get-ChuckNorrisJokeByCategory
--- Source/FreeChuckNorrisJokes.psm1
+++
@@ -1,4 +0,0 @@
-. $PSScriptRoot\Functions\Get-ChuckNorrisJoke
-Export-ModuleMember -Function Get-ChuckNorrisJoke
-. $PSScriptRoot\Functions\Get-ChuckNorrisJokeByCategory
-Export-ModuleMember -Function Get-ChuckNorrisJokeByCategory
